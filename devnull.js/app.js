502a72d6c46fa000feff8086aae83c07dc0c8f1d
Mike Cantrell <mike@devnull.org>
2012-12-22 17:35:34 -0600

    env specific error handlers

--- devnull.js/app.js
+++ devnull.js/app.js
@@ -17,7 +17,6 @@ app.configure(function () {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.favicon());
-    app.use(express.errorHandler());
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
@@ -26,7 +25,13 @@ app.configure(function () {
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
 });
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
 
 /* ------------------ Routes -------------------- */
 app.get('/', routes.index);
