70bc81c15564c46c8a496896d015804f366b7deb
Mike Cantrell <mike@devnull.org>
2012-12-21 22:43:06 -0600

    scoped config

--- devnull.js/config.js
+++ devnull.js/config.js
@@ -2,7 +2,7 @@ var config = {};
 
 
 config.server = {};
-config.server.port = process.env.PORT || 3000;
-config.server.salt = process.env.SALT || 'notsosecret';
+config.server.port = process.env.DEVNULL_JS_PORT || 3000;
+config.server.salt = process.env.DEVNULL_JS_SALT || 'notsosecret';
 
 module.exports = config;
