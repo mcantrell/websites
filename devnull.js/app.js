ce3d6d30ae43df95d2fed75476f8b2172bbaf89f
Mike Cantrell <mike@devnull.org>
2012-12-24 11:50:02 -0600

    arranged express static content before logger to avoid noise

--- devnull.js/app.js
+++ devnull.js/app.js
@@ -17,13 +17,13 @@ app.configure(function () {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.favicon());
-    app.use(express.logger('dev'));
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.logger('default'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(express.cookieParser());
     app.use(express.session({ secret: config.server.salt }));
     app.use(app.router);
-    app.use(express.static(path.join(__dirname, 'public')));
 });
 app.configure('development', function(){
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
