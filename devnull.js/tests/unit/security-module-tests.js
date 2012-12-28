e464aa9420cdd0239a0e82ef3ca1c77f06e3e3f3
Mike Cantrell <mike@devnull.org>
2012-12-28 12:41:56 -0600

    added unit test for configuring google passport strategy

---
+++ devnull.js/tests/unit/security-module-tests.js
@@ -0,0 +1,41 @@
+var should = require("should"),
+    SandboxedModule = require('sandboxed-module'),
+    config = require('../../boot/config.js');
+
+
+describe('Security Service Unit Tests', function () {
+    describe('#forGooglePassport()', function () {
+        var passport = {
+            use: function(strategy, userLookup) {
+                this.strategy = strategy;
+                return this;
+            },
+            serializeUser: function(callback) {
+                config.logger.info("serialization configured");
+            },
+            deserializeUser: function(callback) {
+                config.logger.info("deserialization configured");
+            }
+        };
+        var passportStrategy = {
+            Strategy: function(options, userLookup) {
+                this.options = options;
+                this.userLookup = userLookup;
+            }
+        };
+        var security = SandboxedModule.require('../../lib/security', {
+            requires: {
+                '../../boot/database.js': { },
+                'passport': passport,
+                'passport-google': passportStrategy
+            }
+        });
+
+        it('should register correct strategy with passport', function () {
+            security.forGooglePassport();
+            passport.strategy.options.returnURL.should.equal(config.passport.returnUrl);
+            passport.strategy.options.realm.should.equal(config.passport.realm);
+            should.exist(passport.strategy.userLookup);
+        });
+    });
+});
